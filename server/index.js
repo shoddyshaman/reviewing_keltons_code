b9973c3d7bed46563a1a7ffea387ce7646926fc0
shoddyshaman <djtiwari90@gmail.com>
2021-09-09 15:43:48 -0600

    testing

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json()); 
 
-app.use('/styles', express.static(path.join(__dirname, './styles.css')))
+// app.use('/styles', express.static(path.join(__dirname, './styles.css')))
 
 app.get('/main',(req,res) => {
   res.sendFile(path.join(__dirname, './main.js'))
